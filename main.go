03b21fa96f3f091e2f3573f84ec5bd9f6f406fff
xuzhiqing <xuzhiqing@datafuselabs.com>
2022-05-06 16:23:28 +0800

    add log

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("aaabbbccc")
 	fmt.Println("fbbbb")
+	fmt.Println("aaabbbccc")
+	fmt.Println("fbbbb")
+	fmt.Println("1123")
+	fmt.Println("ddd")
+	fmt.Println("aaafffbbbccc")
+	fmt.Println("fbbbb")
+	fmt.Println("uuu")
+	fmt.Println("uuu")
+	fmt.Println("iii")
+	fmt.Println("fbbuuubb")
+	fmt.Println("775")
+	fmt.Println("55")
 	panic("kkkkk")
 }
